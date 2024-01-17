bc84c4544d32ed0a37a943eb2d0d4cba7f1635af
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-01-17 22:03:03 +0600

    ownProps use cases when using redux connect function

--- redux-with-react-and-react-redux/src/App.jsx
+++ redux-with-react-and-react-redux/src/App.jsx
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux'
 import './App.css'
 import ReduxHooksCounter from './components/counter-with-redux-hook'
 import DynamicCounter from './components/dynamic-counter'
+import VariableCounter from './components/variable-counter'
 import store from './redux/store'
 
 function App() {
@@ -10,7 +11,15 @@ function App() {
     <Provider store={store}>
       {/* <Counter id='1' /> */}
       <ReduxHooksCounter />
+      <hr />
       <DynamicCounter />
+      <hr />
+
+      <VariableCounter />
+      <hr />
+
+      <VariableCounter dynamic />
+
     </Provider>
   )
 }
---
+++ redux-with-react-and-react-redux/src/components/variable-counter.jsx
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux';
+import { decrementCount, incrementCount } from '../redux/counter/actions';
+import { decrementDynamicCount, incrementDynamicCount } from '../redux/dynamic-counter/actions';
+function VariableCounter({
+  counter,
+  incrementCount,
+  decrementCount
+}) {
+  return (
+    <div>
+      <h1>
+        {counter}
+      </h1>
+      <div>
+        <button onClick={incrementCount}>Increment</button>
+        <button onClick={decrementCount}>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps)
+  return {
+    counter: ownProps?.dynamic ? state.dynamicCounter.value : state.counter.value
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    incrementCount: () => ownProps?.dynamic ? dispatch(incrementDynamicCount(5)) : dispatch(incrementCount()),
+    decrementCount: () => ownProps?.dynamic ? dispatch(decrementDynamicCount(2)) : dispatch(decrementCount())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(VariableCounter)
