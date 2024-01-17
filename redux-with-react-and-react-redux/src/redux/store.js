fd8aed35cdcfc5b5c6a558dce38646d89149e1af
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-01-17 23:13:21 +0600

    @redux-devtools/extension

--- redux-with-react-and-react-redux/src/redux/store.js
+++ redux-with-react-and-react-redux/src/redux/store.js
@@ -1,8 +1,13 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import reduxLogger from 'redux-logger';
 import myLogger from './middlewares/my-logger';
 import rootReducer from './root-reducer';
 
-const store = createStore(rootReducer, applyMiddleware(reduxLogger, myLogger));
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxLogger, myLogger))
+);
 
 export default store;
