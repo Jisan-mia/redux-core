f963a43fee7e2dbe496d79c15cc77a62918585de
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-03-27 14:00:26 +0600

    multiple slice

--- redux-toolkit-vanila-js/app/store.js
+++ redux-toolkit-vanila-js/app/store.js
@@ -1,10 +1,11 @@
 const {configureStore} = require('@reduxjs/toolkit')
 const counterSlice = require('../features/counter/counterSlice')
-
+const dynamicCounterSlice = require('../features/dynamicCounter/dynamicCounterSlice');
 // configure store
 const store = configureStore({
   reducer: {
     counter: counterSlice,
+    dynamicCounter: dynamicCounterSlice
   },
 });
 
--- redux-toolkit-vanila-js/index.js
+++ redux-toolkit-vanila-js/index.js
@@ -1,11 +1,15 @@
 const store = require('./app/store');
 const { counterActions } = require('./features/counter/counterSlice');
-
+const {dynamicCounterActions} = require('./features/dynamicCounter/dynamicCounterSlice')
 store.subscribe(() => {
   console.log(store.getState())
 })
 
-store.dispatch(counterActions.incrementCount())
-store.dispatch(counterActions.incrementCount());
-store.dispatch(counterActions.incrementByPayload(2))
-store.dispatch(counterActions.decrementCount());
+// store.dispatch(counterActions.incrementCount())
+// store.dispatch(counterActions.incrementCount());
+// store.dispatch(counterActions.incrementByPayload(2))
+// store.dispatch(counterActions.decrementCount());
+
+store.dispatch(dynamicCounterActions.increment(3));
+store.dispatch(dynamicCounterActions.increment(2));
+store.dispatch(dynamicCounterActions.decrement(3));
